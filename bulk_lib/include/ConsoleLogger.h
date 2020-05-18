6d978cd00d038496c1d6fa9b61d197eb9c801a49
Filipp Gundelakh <f.gundelakh@yandex.ru>
2020-05-18 18:04:19 +0300

    1) Added travis script 2) Added multithreading

--- bulk_lib/include/ConsoleLogger.h
+++ bulk_lib/include/ConsoleLogger.h
@@ -1,13 +1,57 @@
 #ifndef CONSOLE_LOGGER_H
 #define CONSOLE_LOGGER_H
 #include "IObserver.h"
+#include <thread>
+#include <atomic>
+#include <queue>
+#include <condition_variable>
+#include <mutex>
 
 struct ConsoleLogger : public IObserver<std::string>
 {
-    ~ConsoleLogger(){};
+    ConsoleLogger() {
+        m_thread = std::thread(&ConsoleLogger::Run, this);
+    }
+    ~ConsoleLogger(){
+        m_stopped = true;
+        m_condition.notify_all();
+        if(m_thread.joinable()) {
+            m_thread.join();
+        }
+    };
 	virtual void Update(std::string param) override {
-		std::cout << param << std::endl;
+        m_tasks.push(param);
+        m_condition.notify_one();
 	}
+private:
+	void Run(){
+	    while (!m_stopped) {
+            std::unique_lock<std::mutex> lck{m_mutex};
+            while (!m_stopped && m_tasks.empty())
+                m_condition.wait(lck);
+
+            if (m_stopped) {
+                break;
+            }
+            auto param = m_tasks.front();
+            m_tasks.pop();
+            Log(param);
+        }
+        while(!m_tasks.empty()) {
+            auto param = m_tasks.front();
+            m_tasks.pop();
+            Log(param);
+        }
+    }
+    void Log(std::string_view param){
+        std::cout << param.data() << std::endl;
+	}
+private:
+    std::condition_variable m_condition;
+    std::thread m_thread;
+    std::mutex m_mutex;
+    std::queue<std::string> m_tasks;
+    std::atomic<bool> m_stopped{false};
 };
 
 #endif //CONSOLE_LOGGER_H
--- bulk_lib/include/FileLogger.h
+++ bulk_lib/include/FileLogger.h
@@ -1,25 +1,74 @@
 #ifndef FILE_LOGGER_H
 #define FILE_LOGGER_H
 #include <fstream>
+#include <thread>
+#include <atomic>
+#include <queue>
+#include <condition_variable>
+#include <mutex>
+#include <array>
 #include "IObserver.h"
 
 using timepoint = std::chrono::system_clock::time_point;
 struct FileLogger : public IObserver<std::string>, public IObserver<timepoint>
 {
-    ~FileLogger(){};
+    FileLogger(){
+        for(auto& thread : m_thread_pool) {
+            thread = std::thread(&FileLogger::Run, this);
+        }
+    }
+    ~FileLogger(){
+        m_stopped = true;
+        m_condition.notify_all();
+        for(auto& thread : m_thread_pool) {
+            if(thread.joinable()) {
+                thread.join();
+            }
+        }
+    }
 	virtual void Update(std::string param) override {
-		std::ofstream file(filename);
-		if (file.is_open())
-		{
-			file << param;
-			file.close();
-		}
+        m_tasks.push(param);
+        m_condition.notify_one();
 	}
 	virtual void Update(timepoint time) override {
 		filename = "bulk" + std::to_string(time.time_since_epoch().count()) + ".log";
 	}
 private:
+    void Run(){
+        while (!m_stopped) {
+            std::unique_lock<std::mutex> lck{m_mutex};
+            while (!m_stopped && m_tasks.empty())
+                m_condition.wait(lck);
+
+            if (m_stopped) {
+                break;
+            }
+            auto param = m_tasks.front();
+            m_tasks.pop();
+            Log(param);
+        }
+        std::unique_lock<std::mutex> lck{m_mutex};
+        while(!m_tasks.empty()) {
+            auto param = m_tasks.front();
+            m_tasks.pop();
+            Log(param);
+        }
+    }
+    void Log(std::string_view param){
+        std::ofstream file(filename);
+        if (file.is_open())
+        {
+            file << std::this_thread::get_id()<< ":\n" << param.data();
+            file.close();
+        }
+    }
+private:
 	std::string filename;
+    std::condition_variable m_condition;
+    std::array<std::thread,2> m_thread_pool;
+    std::mutex m_mutex;
+    std::queue<std::string> m_tasks;
+    std::atomic<bool> m_stopped{false};
 };
 
 #endif //FILE_LOGGER_H
